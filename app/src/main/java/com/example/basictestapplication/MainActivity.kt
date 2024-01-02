92ad796b325d68f8513ce4aeb2276ea06a037b30
Mikael Meinander <mikael.meinander@qvik.fi>
2024-01-02 09:05:42 +0200

    add ScrollToTop button using derivedStateOf to only recompose when it's needed

--- app/src/main/java/com/example/basictestapplication/MainActivity.kt
+++ app/src/main/java/com/example/basictestapplication/MainActivity.kt
@@ -3,22 +3,31 @@ package com.example.basictestapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.basictestapplication.ui.theme.BasicTestApplicationTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -43,34 +52,88 @@ class MainActivity : ComponentActivity() {
 fun MainScreenContent(vm: MainViewModel) {
     val data = vm.data.collectAsState(initial = emptyList()).value
 
-    Column(
+    val listState = rememberLazyListState()
+    val scope = rememberCoroutineScope()
+
+    Box(
         modifier = Modifier
             .testTag("MainScreen")
             .fillMaxSize()
     ) {
-        Text(
-            text = "data: $data",
-            modifier = Modifier.testTag("DataTextField")
-        )
-        Button(
-            modifier = Modifier
-                .fillMaxWidth()
-                .testTag("CacheButton")
-                .wrapContentHeight(),
-            onClick = { vm.setDataToCache() }) {
-            Text("Set cache value")
+        Column {
+
+            Text(
+                text = "data: $data",
+                modifier = Modifier.testTag("DataTextField")
+            )
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .testTag("CacheButton")
+                    .wrapContentHeight(),
+                onClick = { vm.setDataToCache() }) {
+                Text("Set cache value")
+            }
+            LazyColumn(
+                state = listState,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .testTag("SampleList"),
+
+                content = {
+                    items(50, key = { it }) { index ->
+                        ListItem(index = index)
+                    }
+                    item {
+                        FooterItem()
+                    }
+                }
+            )
         }
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .testTag("SampleList")
+
+        val showButton by remember {
+            derivedStateOf {
+                listState.firstVisibleItemIndex > 0
+            }
+        }
+
+        AnimatedVisibility(
+            modifier = Modifier.align(Alignment.BottomEnd),
+            visible = showButton
         ) {
-            items(50) { index ->
-                Text(text = "${index + 1}", Modifier.testTag("position=$index"))
-            }
-            item {
-                Text("Footer", modifier = Modifier.testTag("FooterTextField"))
-            }
+            ScrollToTopButton(
+                modifier = Modifier,
+                onClick = {
+                    scope.launch {
+                        // Animate scroll to the first item
+                        listState.animateScrollToItem(index = 0)
+                    }
+                }
+            )
         }
     }
+}
+
+@Composable
+fun ScrollToTopButton(modifier: Modifier, onClick: () -> Unit) {
+    Button(modifier = modifier, onClick = onClick) {
+        Text("Scroll to top")
+    }
+}
+
+
+@Composable
+fun ListItem(index: Int) {
+    Text(
+        text = "${index + 1}",
+        modifier = Modifier.testTag("position=$index")
+    )
+}
+
+@Composable
+fun FooterItem() {
+    Text(
+        text = "Footer",
+        modifier = Modifier.testTag("FooterTextField")
+    )
 }
